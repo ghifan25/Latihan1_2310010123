b897718b8bdec79b00e0a5ec3da2eeae5641013a
ghifan25 <ghifan2134@gmail.com>
2025-06-11 05:33:12 +0800

    Membuat class MatematikaBeraksi

--- src/hitung/MatematikaBeraksi.java
+++ src/hitung/MatematikaBeraksi.java
@@ -1,5 +1,13 @@
 package hitung;
 
 public class MatematikaBeraksi {
-    
+    public static void main(String[] args) {
+        //mebuat objek
+        Matematika ghifan = new Matematika(2,3);
+        
+        System.out.println("Hasil Penjumlahan : "+ghifan.setPenjumlahan());
+        System.out.println("Hasil Pengurangan : "+ghifan.setPengurangan());
+        System.out.println("Hasil Perkalian : "+ghifan.setPerkalian());
+        System.out.println("Hasil Pembagian : "+ghifan.setPembagian());
+    }
 }
